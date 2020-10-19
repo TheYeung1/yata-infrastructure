923c549206161ab21c114ad17b05f4ce26dc7c91
Winston Yeung <yeungw@Winstons-MacBook.local>
2020-10-18 23:42:56 -0700

    Define ListTable and ListItemTable

--- lib/yata-infrastructure-stack.ts
+++ lib/yata-infrastructure-stack.ts
@@ -1,9 +1,26 @@
 import * as cdk from '@aws-cdk/core';
+import * as dynamo from '@aws-cdk/aws-dynamodb';
 
 export class YataInfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const listTable = new dynamo.Table(this, 'ListTable', {
+      partitionKey: {
+        name: "UserId",
+        type: dynamo.AttributeType.STRING
+      },
+      readCapacity: 1,
+      writeCapacity: 1
+    });
+
+    const listItemTable = new dynamo.Table(this, 'ListItemTable', {
+      partitionKey: {
+        name: "ListId",
+        type: dynamo.AttributeType.STRING
+      },
+      readCapacity: 1,
+      writeCapacity: 1
+    });  
   }
 }
